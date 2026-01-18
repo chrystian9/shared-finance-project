8c7fd06da8d0a22d90452edd08ea5b55a269b62e
Chrystian Amaral <chrys.a.amaral@gmail.com>
2026-01-18 19:45:25 -0300

    app service

---
+++ SharedFinanceConsole/SharedFinanceConsole/Application/SharedFinanceAppService.cs
@@ -0,0 +1,51 @@
+﻿using SharedFinanceConsole.Domain.Aggregates.AccountAggregate;
+using SharedFinanceConsole.Domain.Aggregates.AccountAggregate.ValueObjects;
+using SharedFinanceConsole.Domain.Aggregates.UserAggregate;
+
+namespace SharedFinanceConsole.Application
+{
+    public class SharedFinanceAppService
+    {
+        public readonly IDictionary<Guid, User> _usersById = new Dictionary<Guid, User>();
+        public readonly IDictionary<Guid, Account> _accountsByUserId = new Dictionary<Guid, Account>();
+
+        public Guid AddUser(string name)
+        {
+            var user = new User(name);
+
+            _usersById.Add(user.Id, user);
+
+            return user.Id;
+        }
+
+        public Guid AddAccount(Guid userId)
+        {
+            var account = new Account(userId);
+
+            _accountsByUserId.Add(account.UserId, account);
+
+            return account.Id;
+        }
+
+        public void RegisterExpense(Guid payerUserId,
+            decimal totalValue,
+            string description,
+            IEnumerable<TransactionCounterparty> counterparties)
+        {
+            var payerAccount = _accountsByUserId[payerUserId];
+
+            payerAccount.RegisterExpense(totalValue, description, counterparties);
+
+            foreach (var counterparty in counterparties)
+            {
+                var counterpartyAccount = _accountsByUserId[counterparty.UserId];
+
+                counterpartyAccount.RegisterTransfer(
+                    counterparty.GetValue(totalValue),
+                    description,
+                    counterparty.UserId
+                );
+            }
+        }
+    }
+}
